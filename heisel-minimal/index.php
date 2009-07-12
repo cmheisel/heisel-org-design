59fc4b3bdf38294f92cbd935ebf1a9f4ec50bb3d
Chris Heisel <chris@heisel.org>
2009-07-12 13:03:18 -0400

    Added elsewhere section

--- heisel-minimal/index.php
+++ heisel-minimal/index.php
@@ -28,7 +28,7 @@
     $category_id = get_cat_ID('Blogmarks');
     $category_link = get_category_link($category_id);
     ?>
-    <h4><a href="<?php echo $category_link; ?>">Posts &raquo;</a></h4>
+    <h4><a href="<?php echo $category_link; ?>">Blogmarks &raquo;</a></h4>
 <?php while ( $links_query->have_posts() ) : $links_query->the_post(); ?>
     
     <?php
@@ -46,6 +46,23 @@
 <?php
     $args = array(
         'category_name' => "Code",
+        'title_li' => '',
+        'title_before' => '',
+        'title_after' => '',
+        'categorize' => 0,
+        'show_description' => 1,
+        'between' => ' &ndash; ', 
+    );
+    wp_list_bookmarks($args);
+?>
+</ul>
+
+<!-- CODE -->
+<h4>Elsewhere</h4>
+<ul>
+<?php
+    $args = array(
+        'category_name' => "Elsewhere",
         'title_li' => '',
         'title_before' => '',
         'title_after' => '',
