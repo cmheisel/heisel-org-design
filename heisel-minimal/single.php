c16c5ba2ca3ba6431b591cf289328076ee25a6d1
Chris Heisel <chris@heisel.org>
2009-07-12 17:06:01 -0400

    Added content

--- heisel-minimal/single.php
+++ heisel-minimal/single.php
@@ -6,6 +6,8 @@
 
 <h2><?php the_title(); ?></h2>
 
+<?php the_content('<p>Read the rest of this entry &raquo;</p>'); ?>
+
 <?php endwhile; else: ?>
     <p>Sorry, no posts matched your criteria.</p>
 <?php endif; ?>
