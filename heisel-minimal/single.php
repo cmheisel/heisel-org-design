cd8d868c4fdc571a7cecd6f79914638ab1421346
Chris Heisel <chris@heisel.org>
2009-07-12 15:45:28 -0400

    Added date line

--- heisel-minimal/single.php
+++ heisel-minimal/single.php
@@ -1,3 +1,11 @@
 <?php get_header(); ?>
 
+<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+
+<h4><a href="<?php bloginfo('home'); ?>/<?php the_time('Y')?>/<?php the_time('m')?>/"><?php echo the_date("F j, Y"); ?></a></h4>
+
+<?php endwhile; else: ?>
+    <p>Sorry, no posts matched your criteria.</p>
+<?php endif; ?>
+
 <?php get_footer(); ?>
