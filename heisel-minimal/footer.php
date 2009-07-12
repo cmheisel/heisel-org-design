dfffa4a2aa86693ceb4f0b52c4e56c84b8d642c2
Chris Heisel <chris@heisel.org>
2009-07-12 11:37:55 -0400

    Added footer

--- heisel-minimal/footer.php
+++ heisel-minimal/footer.php
@@ -1,3 +1,10 @@
+            <!--CONTENT STOP-->
+            </div>
+            
+            <div class="span-12 prepend-6 append-6 last clear" id="footer">
+                <a href="/blog/contact/">Contact</a> &mdash; Copyright &copy; <a href="/blog/about/"><?php wp_list_authors("exclude_admin=0&show_fullname=1&html=0")?></a> <?php echo date("Y"); ?><br>
+                Views expressed do not necessarily reflect those of my employer.<br>
+            </div>
         </div>
     </body>
 </html>
