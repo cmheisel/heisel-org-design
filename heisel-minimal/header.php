07e11612b810a1ef3fdba7133409619f7c86199c
Chris Heisel <chris@heisel.org>
2009-07-12 11:13:36 -0400

    Adding header/footer

---
+++ heisel-minimal/header.php
@@ -0,0 +1,16 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN"
+"http://www.w3.org/TR/html4/strict.dtd">
+<html lang="en">
+    <head>
+        <title>Chris Heisel</title>
+
+        <meta http-equiv="Content-Type" content="text/html;charset=utf-8">
+
+        <link rel="stylesheet" href="<?php bloginfo('stylesheet_directory'); ?>/css/blueprint/screen.css" type="text/css" media="screen, projection">
+        <link rel="stylesheet" href="<?php bloginfo('stylesheet_directory'); ?>/css/blueprint/print.css" type="text/css" media="print">	
+        <!--[if lt IE 8]><link rel="stylesheet" href="<?php bloginfo('stylesheet_directory'); ?>/css/blueprint/ie.css" type="text/css" media="screen, projection"><![endif]-->
+
+        <link rel="stylesheet" href="<?php bloginfo('stylesheet_directory'); ?>/css/master.css" type="text/css" media="screen, projection">
+    </head>
+    <body>
+        <div class="container">
