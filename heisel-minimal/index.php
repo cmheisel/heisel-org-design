4cc92d241d05655888b525cf3a8726f12a772a38
Chris Heisel <chris@heisel.org>
2009-07-12 12:08:01 -0400

    Got first post showing.

--- heisel-minimal/index.php
+++ heisel-minimal/index.php
@@ -1,3 +1,14 @@
 <?php get_header(); ?>
 
+<?php $posts_query = new WP_Query('cat=-2&posts_per_page=1')?>
+<?php if($posts_query->have_posts()) : ?>
+    <h4><a href="<?php bloginfo('home'); ?>/<?php the_time('Y')?>/">Posts &raquo;</a></h4>
+<?php while ( $posts_query->have_posts() ) : $posts_query->the_post(); ?>
+    <h2><a href="<?php the_permalink(); ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2>
+
+    <?php the_excerpt(); ?>
+<?php endwhile; ?>
+<?php endif; ?>
+
+
 <?php get_footer(); ?>
