59e3f45ca755ed295159eb09cdf1dc034702a0d4
Chris Heisel <chris@heisel.org>
2009-07-12 15:53:42 -0400

    Got dateline and headline on post

--- heisel-minimal/single.php
+++ heisel-minimal/single.php
@@ -2,7 +2,9 @@
 
 <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 
-<h4><a href="<?php bloginfo('home'); ?>/<?php the_time('Y')?>/<?php the_time('m')?>/"><?php echo the_date("F j, Y"); ?></a></h4>
+<h4><a href="<?php bloginfo('home'); ?>/<?php the_time('Y'); ?>/<?php the_time('m'); ?>/" title="More in <?php the_time('F'); ?>"><?php the_date("F j, Y"); ?></a></h4>
+
+<h2><?php the_title(); ?></h2>
 
 <?php endwhile; else: ?>
     <p>Sorry, no posts matched your criteria.</p>
