666a51f271bf1ae5f536fd46b0f24383e11120fd
Chris Heisel <chris@heisel.org>
2009-07-12 17:38:05 -0400

    Added related entries

---
+++ heisel-minimal/yarpp-template-example.php
@@ -0,0 +1,8 @@
+<?php if ($related_query->have_posts()):?>
+<h4>Related Posts</h4>
+<ul>
+    <?php while ($related_query->have_posts()) : $related_query->the_post(); ?>
+    <li><a href="<?php the_permalink() ?>" rel="bookmark"><?php the_title(); ?></a></li>
+    <?php endwhile; ?>
+</ul>
+<?php endif; ?>
