34a230b076961a909fe46a11e57a31fb36bbc789
Chris Heisel <chris@heisel.org>
2009-07-18 15:35:46 -0400

    Added title to post >> link

--- heisel-minimal/index.php
+++ heisel-minimal/index.php
@@ -3,7 +3,7 @@
 <!-- POSTS -->
 <?php $posts_query = new WP_Query('cat=-2&posts_per_page=1')?>
 <?php if($posts_query->have_posts()) : ?>
-    <h4><a href="<?php bloginfo('home'); ?>/<?php the_time('Y')?>/">Posts &raquo;</a></h4>
+    <h4><a href="<?php bloginfo('home'); ?>/<?php the_time('Y')?>/" title="Posts from <?php the_time('Y')?>">Posts &raquo;</a></h4>
 <?php while ( $posts_query->have_posts() ) : $posts_query->the_post(); ?>
     <h2><a href="<?php the_permalink(); ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2>
 
