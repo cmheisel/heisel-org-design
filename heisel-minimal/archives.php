d260e9bcb502e5cfa57223d776c861004a22838b
Chris Heisel <chris@heisel.org>
2009-07-18 15:35:39 -0400

    Added yearly archives

--- heisel-minimal/archives.php
+++ heisel-minimal/archives.php
@@ -1,9 +1,14 @@
 <?php get_header(); ?>
 
+<h4>By year</h4>
+<ul>
+	<?php wp_get_archives('type=yearly'); ?>
+</ul>
+
 <h4>By subject</h4>
 	<ul>
 		 <?php
-		    $args = array('title_li' => '');
+		    $args = array('title_li' => '', 'exclude' => '2');
 		    wp_list_categories( $args ); 
 		 ?>
 	</ul>
