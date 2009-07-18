192ae42da4903a679fc9679fba75d7c572659706
Chris Heisel <chris@heisel.org>
2009-07-18 15:31:45 -0400

    Combined category and monthly archive.

--- heisel-minimal/archive.php
+++ heisel-minimal/archive.php
@@ -4,17 +4,33 @@
 global $wp_query;
 query_posts(
 	array_merge(
-		array('cat' => "-2", "posts_per_page" => "10"),
+		array('cat' => -2),
 		$wp_query->query
 	)
 );
 ?>
 
 <?php if(have_posts()) : ?>
+    
+    <?php if( is_month() ): ?>
+        <h3><?php the_time('F Y'); ?>
+    <?php elseif( is_day() ): ?>
+        <!-- pass -->
+    <?php elseif( is_category() || is_archive() ): ?>
+        <h3><?php wp_title('', true, ''); ?></h3>
+    <?php endif ?>
+    
     <?php while ( have_posts() ) : the_post(); ?>
-        <h4><a href="<?php bloginfo('home'); ?>/<?php the_time('Y'); ?>/<?php the_time('m'); ?>/" title="More in <?php the_time('F'); ?>"><?php the_date("F j, Y"); ?></a></h4>
-        <h2><a href="<?php the_permalink(); ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2>
-        <?php the_excerpt(); ?>
+        <?php if(!is_category('Blogmarks') || is_archive() ): ?>
+            <h4><a href="<?php bloginfo('home'); ?>/<?php the_time('Y'); ?>/<?php the_time('m'); ?>/" title="More in <?php the_time('F'); ?>"><?php the_date("F j, Y"); ?></a></h4>
+            <h2><a href="<?php the_permalink(); ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2>
+            <?php the_excerpt(); ?>
+        <?php else: ?>
+            <?php $post_custom = get_post_custom(); ?>
+            <p>
+                <a href="<?php echo $post_custom["postalicious_href"][0] ?>" title="Original post"><?php echo $post_custom['postalicious_title'][0] ?></a> &ndash; <?php echo $post_custom['postalicious_description'][0] ?> <a href="<?php the_permalink(); ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>">&raquo;</a>
+            </p>
+        <?php endif;?>
     <?php endwhile; ?>
 <?php endif; ?>
 
